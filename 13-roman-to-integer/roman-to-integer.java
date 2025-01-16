47a39f5945e07cc277083be67d7598ebdcb59811
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2025-01-16 15:31:41 +0700

    Time: 4 ms (71.12%) | Memory: 44.9 MB (34.91%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,26 +1,31 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> roman = new HashMap<String, Integer>();
-
-        roman.put("I", 1);
-        roman.put("V", 5);
-        roman.put("X", 10);
-        roman.put("L", 50);
-        roman.put("C", 100);
-        roman.put("D", 500);
-        roman.put("M", 1000);
+      Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
 
         int total = 0;
+        int prevValue = 0;
 
-        for (int i = 0; i < (s.length() - 1); i++) {
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = roman.get(s.charAt(i));
 
-            if (roman.get(String.valueOf(s.charAt(i))) < roman.get(String.valueOf(s.charAt(i + 1)))) {
-                total -= roman.get(String.valueOf(s.charAt(i)));
+            // If the current value is less than the previous value, subtract it.
+            if (currentValue < prevValue) {
+                total -= currentValue;
             } else {
-                total += roman.get(String.valueOf(s.charAt(i)));
+                total += currentValue;
             }
+
+            // Update the previous value.
+            prevValue = currentValue;
         }
 
-        return total + roman.get(String.valueOf(s.charAt(s.length() - 1)));
+        return total;
     }
 }
